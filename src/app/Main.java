4045214688c10ece053dfecd50dc644233dd00eb
Demétrio Coutinho <demetriorc8@gmail.com>
2025-11-10 22:30:35 -0300

    feat(Main): Criação do fluxo de menus principal.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,16 +1,34 @@
 package app;
 
 import controller.BibController;
-import servirces.BibServices;
 import view.MenusView;
 
 public class Main {
     public static void main(String[] args) {
 
         BibController bibController = new BibController();
-        BibServices services = new BibServices();
         MenusView menus = new MenusView();
-        bibController.analisarDados();
 
+        int opc;
+        do {
+            opc = menus.menuPrincipal();
+
+            switch (opc) {
+                case 1:
+                    bibController.importacaoDaBase();
+                    break;
+                case 2:
+                    bibController.analisarDados();
+                    break;
+                case 3:
+                    bibController.gerenciarLogs();
+                    break;
+                case 0:
+                    System.out.println("Saindo do BibSync... Até logo!");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opc != 0);
     }
 }
